baf9010c23a6c71f07413d6c60867c42ffe58b26
Laurent Sartran <lsartran@gmail.com>
2017-06-06 08:12:39 +0100

    Learn at last. Very messy and hacky.

--- tictactoe-q-learning/tictactoe.py
+++ tictactoe-q-learning/tictactoe.py
@@ -16,6 +16,8 @@ TODO:
 
 #from enum import IntEnum
 import random
+import sys
+import operator
 
 import tqdm
 
@@ -122,10 +124,20 @@ class RandomPlayer(object):
     def __init__(self, square):
         self.square = square
 
-    def play(self, grid):
+    #def play(self, grid):
+    #    empty_squares = [(i,j) for (i,j),sq in grid.items() if sq == Square.EMPTY]
+    #    pos = random.choice(empty_squares)
+    #    return pos
+
+    @staticmethod
+    def play(grid):
         empty_squares = [(i,j) for (i,j),sq in grid.items() if sq == Square.EMPTY]
         pos = random.choice(empty_squares)
         return pos
+
+    def feedback(self, *args, **kwargs):
+        # yeah sure, whatever dude
+        pass
 
 
 class RandomStable(object):
@@ -133,44 +145,105 @@ class RandomStable(object):
         return RandomPlayer(square)
 
 
-class LearningStable(object);
+class LearningStable(object):
     def __init__(self):
-        self.quality = {}
+        self.quality = {} # (sq, grid: [(action, quality)])
+        self.threshold = 0.7
 
     def player(self, square):
         # XXX beware of different players using the same quality but playing with X instead of O!
-        return LearningPlayer(square, quality)
+        return LearningPlayer(square, self)
 
 
 class LearningPlayer(object):
-    def __init__(self, square, quality):
-        raise NotImplementedError
+    def __init__(self, square, team):
+        self.square = square
+        self.team = team
+        self.quality = team.quality
+        self.alpha = 0.1
+        self.discount = 0.9
+        self.has_played = False
+
+    def play(self, grid):
+        self.has_played = True
+        s_t = (self.square, int(grid))
+        if s_t not in self.quality:
+            self.quality[s_t] = {(i,j):0.00 for i in range(3) for j in range(3)}
+        if random.random() <= self.team.threshold:
+            a_t,q = max(self.quality[s_t].items(),key=operator.itemgetter(1))
+            if self.square == Square.NOUGHT and q > 0.0 and q < 1.0 and grid.num_sq == 6:
+                pass
+        else:
+            a_t = RandomPlayer.play(grid)
+            #print("Playing with {}".format(str(self.square)))
+            #print(str(grid))
+            #print(a_t,q)
+        self.s_t = s_t
+        self.a_t = a_t
+        return a_t
+
+    def feedback(self, reward, new_grid):
+        if not self.has_played:
+            return
+        s_tp1 = (self.square, int(new_grid))
+        max_Q_s_tp1 = 0.0 if not s_tp1 in self.quality else max(self.quality[s_tp1].values())
+        #if max_Q_s_tp1 > 0:
+        #    print(self.quality[s_tp1])
+        self.quality[self.s_t][self.a_t] += self.alpha * (reward + self.discount * max_Q_s_tp1 - self.quality[self.s_t][self.a_t])
+
+    def fatal(self, reward):
+        if self.s_t not in self.quality:
+            self.quality[self.s_t] = {(i,j):0.0 for i in range(3) for j in range(3)}
+        self.quality[self.s_t][self.a_t] += reward 
 
 
 def battle(player1, player2):
+    assert player1.square == Square.NOUGHT
+    assert player2.square == Square.CROSS
+
     grid = Grid()
 
     while not grid.full():
         pos = player1.play(grid)
         #print("Player 1",pos)
-        grid[pos] = Square.NOUGHT
+        try:
+            grid[pos] = player1.square
+        except InvalidTicTacToeMove:
+            player1.fatal(-100)
+            return player2.square
 
         w = grid.winner()
         if w:
+            player1.feedback(1.0 if w == player1.square else -1.0, grid)
+            player2.feedback(1.0 if w == player2.square else -1.0, grid)
             #print("{} won!".format(str(w)))
             break
+        else:
+            player1.feedback(-0.1, grid)
+            player2.feedback(+0.1, grid)
+
 
         if not grid.full():
             pos = player2.play(grid)
             #print("Player 2",pos)
-            grid[pos] = Square.CROSS
+            try:
+                grid[pos] = player2.square
+            except InvalidTicTacToeMove:
+                player2.fatal(-100)
+                return player1.square
 
             w = grid.winner()
             if w:
+                player1.feedback(1.0 if w == player1.square else -1.0, grid)
+                player2.feedback(1.0 if w == player2.square else -1.0, grid)
                 #print("{} won!".format(str(w)))
                 break
+            else:
+                player1.feedback(+0.1, grid)
+                player2.feedback(-0.1, grid)
     else:
-        #print("Draw")
+        player1.feedback(-0.5, grid)
+        player2.feedback(-0.5, grid)
         w = False
 
     #print(str(grid))
@@ -178,22 +251,63 @@ def battle(player1, player2):
 
 if __name__ == '__main__':
     random.seed(42)
-    results = {False: 0, Square.NOUGHT: 0, Square.CROSS: 0}
-    num_games = 10000
-    s1 = RandomStable()
-    s2 = RandomStable()
+    results = {False: 0, 1: 0, 2: 0} # draw, A, B
+    num_games = int(sys.argv[1]) if len(sys.argv) >= 2 else 10000
+    team_a = LearningStable()
+    team_b = RandomStable()
     for _ in tqdm.tqdm(range(num_games)):
-        for (sq1, sq2) in [(Square.NOUGHT,Square.CROSS),(Square.CROSS, Square.NOUGHT)]:
-            p1 = s1.player(sq1)
-            p2 = s2.player(sq2)
-            res = battle(p1,p2)
+        for i in {1,2}:
+            player_a = team_a.player(Square.NOUGHT if i == 1 else Square.CROSS)
+            player_b = team_a.player(Square.CROSS if i == 1 else Square.NOUGHT)
+            if i == 1:
+                res = battle(player_a,player_b)
+            else:
+                res = battle(player_b,player_a)
             if res is False:
                 results[False] += 1
-            elif res is sq1:
-                results[1] += 1
-            elif res is sq2:
-                results[2] += 1
+            elif res == Square.NOUGHT:
+                results[i] += 1
+            elif res == Square.CROSS:
+                results[3-i] += 1
+            else:
+                raise Exception
 
+        if _ > 0 and ((_ % 10000) == 0):     
+            for k,v in results.items():
+                print("{}:\t{}".format(k,0.5*v/_))
+            print(len(team_a.quality),len(WIN_CACHE))
     for k,v in results.items():
-        print("{}:\t{}".format(k,0.5*v/num_games))
+        print("{}:\t{}".format(k,0.5*v/_))
 
+    print(team_a.quality[(Square.NOUGHT,1-3)])
+
+    team_a.threshold = 1.0
+    results = {False: 0, 1: 0, 2: 0} # draw, A, B
+
+    for _ in tqdm.tqdm(range(num_games)):
+        for i in {1}:
+            player_a = team_a.player(Square.NOUGHT if i == 1 else Square.CROSS)
+            player_b = team_a.player(Square.CROSS if i == 1 else Square.NOUGHT)
+            if i == 1:
+                res = battle(player_a,player_b)
+            else:
+                res = battle(player_b,player_a)
+            if res is False:
+                results[False] += 1
+            elif res == Square.NOUGHT:
+                results[i] += 1
+            elif res == Square.CROSS:
+                results[3-i] += 1
+            else:
+                raise Exception
+
+        if _ > 0 and ((_ % 10000) == 0):     
+            for k,v in results.items():
+                print("{}:\t{}".format(k,1.0*v/10000))
+                results = {False: 0, 1: 0, 2: 0} # draw, A, B
+            print(len(team_a.quality),len(WIN_CACHE))
+    for k,v in results.items():
+        print("{}:\t{}".format(k,1.0*v/_))
+
+    print(team_a.quality[(Square.NOUGHT,1-3)])
+    print(team_a.quality[(Square.CROSS,1-3+3**8)])
